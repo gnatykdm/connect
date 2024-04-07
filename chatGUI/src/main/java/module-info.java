362558329f7da6b0417d3b4149bd02db66130891
gnatyk-dmytro <gnatykwork@gmail.com>
2024-04-07 20:50:19 +0200

    chatFlow-start

---
+++ chatGUI/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.chatgui.chatgui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.chatgui.chatgui to javafx.fxml;
+    exports org.chatgui.chatgui;
+}
