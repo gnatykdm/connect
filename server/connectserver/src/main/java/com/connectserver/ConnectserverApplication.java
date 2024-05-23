74d5216a70c9ecad5b90835e9ac413c1609a73e1
gnatyk-dmytro <gnatykwork@gmail.com>
2024-05-23 20:12:18 +0200

    websocket-server

---
+++ server/connectserver/src/main/java/com/connectserver/ConnectserverApplication.java
@@ -0,0 +1,13 @@
+package com.connectserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectserverApplication.class, args);
+	}
+
+}
---
+++ server/connectserver/src/test/java/com/connectserver/ConnectserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
